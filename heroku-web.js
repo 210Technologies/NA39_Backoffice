a3cc90b276557cc6d84717a75f470ccc2f3ae967
USKHolding <contact@us-onlineservices.com>
2017-05-19 20:13:01 +0200

    add workouts request`

--- heroku-web.js
+++ heroku-web.js
@@ -6,6 +6,6 @@ var app = express();
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/build"));
 app.get('/', function (req, res) {
-  res.sendfile('/build/index.html')
+  res.sendfile('./build/index.html')
 })
 app.listen(process.env.PORT || 5000);
