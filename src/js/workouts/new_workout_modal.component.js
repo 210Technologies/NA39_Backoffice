d32256ee9d616fc71f0bfab0cd9a85e23d08a359
YoucefSFPlus <youcefus@us-canteen.com>
2018-03-29 20:06:56 +0200

    V 1.1.0: Gestion workout et contenues payant

--- src/js/workouts/new_workout_modal.component.js
+++ src/js/workouts/new_workout_modal.component.js
@@ -7,7 +7,7 @@ class NewWorkoutModalCtrl {
     this._AppConstants = AppConstants;
     this._showEdit = false
     this._new_workout = {level: 'easy', status: 'online'}
-    
+
   }
 
   saveWorkout(){
@@ -18,9 +18,10 @@ class NewWorkoutModalCtrl {
       item.formData.push({'workout[title]': this._new_workout.title})
       item.formData.push({'workout[description]': this._new_workout.description})
       item.formData.push({'workout[status]': this._new_workout.status})
+      item.formData.push({'workout[need_subsription]': this._new_workout.need_subsription})
       item.alias = 'workout[cover]'
       item.method = 'POST'
-      
+
       item.onProgress = function(progress){
         item.progress = progress
         if (progress == 100)
